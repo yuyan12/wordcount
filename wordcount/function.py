c560fe84fa781154aaf6c29c5345d08c659ee8a6
cai_ <390226569@qq.com>
2019-05-07 10:26:16 +0800

    添加了“关于”页面

--- wordcount/function.py
+++ wordcount/function.py
@@ -12,3 +12,7 @@ def count(request):
     #return render(request, 'count.html')#render可以向html中传递信息，使用字典的方式
     return render(request, 'count.html',
                  {'count': total_count, 'text':user_text})#render可以向html中传递信息，使用字典的方式
+                 
+
+def about(request):
+    return render(request, 'about.html')
