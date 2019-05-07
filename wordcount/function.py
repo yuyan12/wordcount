8a61ee13bc441c725693d720fab3a57aed681182
cai_ <390226569@qq.com>
2019-05-07 19:56:19 +0800

    界面修改v1.0（功能满足）

--- wordcount/function.py
+++ wordcount/function.py
@@ -9,10 +9,14 @@ def home(request):
 def count(request):
     total_count = len(request.GET['text']) #text是字典形式的
     user_text = request.GET['text']
+    en1_1 = request.GET['en1']
+    en2_1 = request.GET['en2']
     #return render(request, 'count.html')#render可以向html中传递信息，使用字典的方式
     return render(request, 'count.html',
-                 {'count': total_count, 'text':user_text})#render可以向html中传递信息，使用字典的方式
+                 {'count': total_count, 'text': user_text,'en1': en1_1, 'en2': en2_1})
+                 #render可以向html中传递信息，使用字典的方式
                  
 
 def about(request):
     return render(request, 'about.html')
+
