53ab0b9db0becb7539e6f710698ba44082bfc79e
cai_ <390226569@qq.com>
2019-05-16 21:55:08 +0800

    代码调整
    发现部署有冲突？待解决

--- wordcount/ten_test.py
+++ wordcount/ten_test.py
@@ -6,15 +6,24 @@ sys.path.append('./model/')
 import single_judge
 
 
+'''
+en1_1 = 'A'
+en2_1 = 'B'
+text = 'AandB'
+ans = 'null'
+ans = single_judge.single_judge(en1_1, en2_1, text)
+#sess = tf.Session()
 
-def main(_):
-    en1_1 = 'A'
-    en2_1 = 'B'
-    text = 'AandB'
+'''
+def main1(en1_1,en2_1,text):
     ans = 'null'
     ans = single_judge.single_judge(en1_1, en2_1, text)
-    print(ans)
+
+main1('A','B','AdfjskldfjklB')
+
+'''
 
 
 if __name__ == "__main__":
     tf.app.run()
+'''
